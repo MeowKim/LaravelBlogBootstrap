721631d905bff73eb81cf3639dd43fcfa0b3e0d2
MeowKim <ariesmilk51@gmail.com>
2020-03-31 13:43:25 +0900

    change to resource routing

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,7 @@ Route::get('/', function () {
     return redirect()->route('articles.index');
 });
 
-// 블로그 게시물
-// Route::group(['prefix' => 'articles'], function () {
-//     Route::get('', 'ArticleController@index')->name('articles.index');  // 목록
-//     Route::get('create', 'ArticleController@create')->name('articles.create');  // 작성 폼
-//     Route::post('', 'ArticleController@store')->name('articles.store'); // 저장
-//     Route::get('{id}', 'ArticleController@show')->name('articles.show');    // 상세
-//     Route::get('{id}/edit', 'ArticleController@edit')->name('articles.edit');   // 수정 폼
-//     Route::put('{id}', 'ArticleController@update')->name('articles.update');    // 업데이트
-//     Route::delete('{id}', 'ArticleController@destroy')->name('articles.destroy');   // 삭제
-// });
-
+// 게시물
 Route::resource('/articles', 'ArticleController');
 
 // 사용자 인증
