a5bdd438f92edb3238095e83119281a5180ff606
MeowKim <ariesmilk51@gmail.com>
2020-04-10 11:27:27 +0900

    merge migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('user_id', 20)->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin')->default(false);
+            $table->string('image', 255)->nullable();
+            $table->string('image_name', 255)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2020_03_27_020707_create_articles_table.php
+++ database/migrations/2020_03_27_020707_create_articles_table.php
@@ -17,6 +17,11 @@ class CreateArticlesTable extends Migration
             $table->bigIncrements('id');
             $table->string('title', 255);
             $table->longText('content');
+            $table->string('image_name', 255)->nullable();
+            $table->string('image', 255)->nullable();
+            $table->string('created_by', 20);
+            $table->string('updated_by', 20);
+
             $table->timestamps();
             $table->boolean('is_published')->nullable()->default(false);
         });
