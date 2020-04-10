6628ea2a1a9ff18b91e8af8e90108f3df405eeb8
MeowKim <ariesmilk51@gmail.com>
2020-04-10 17:08:41 +0900

    separate manipulations to keep original one

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,14 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('user_id', 20);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('is_admin')->default(false);
-            $table->string('image', 255)->nullable();
-            $table->string('image_name', 255)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2020_04_10_150000_modify_users_table.php
+++ database/migrations/2020_04_10_150000_modify_users_table.php
@@ -16,7 +16,10 @@ class ChangePkToUserIdFromUsers extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->unique('id');
             $table->dropPrimary();
-            $table->primary('user_id');
+            $table->string('user_id', 20)->after('id')->primary();
+            $table->string('image_name', 255)->nullable()->after('password');
+            $table->string('image', 255)->nullable()->after('password');
+            $table->boolean('is_admin')->after('password')->default(false);
         });
     }
 
@@ -28,7 +31,13 @@ class ChangePkToUserIdFromUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->dropPrimary();
             $table->primary('id');
+            $table->dropUnique('users_unique_id');
+            $table->dropColumn('user_id');
+            $table->dropColumn('image_name');
+            $table->dropColumn('image');
+            $table->dropColumn('is_admin');
         });
     }
 }
