427aa664dba5fe008acda8c04a67dc6492d6c031
MeowKim <ariesmilk51@gmail.com>
2020-04-10 17:01:09 +0900

    fix issue

--- database/migrations/2020_04_10_165632_change_pk_to_user_id_from_users.php
+++ database/migrations/2020_04_10_165632_change_pk_to_user_id_from_users.php
@@ -14,6 +14,7 @@ class ChangePkToUserIdFromUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->unique('id');
             $table->dropPrimary();
             $table->primary('user_id');
         });
