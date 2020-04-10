0b9d06c16f57bd7ace8f11e4b8075e7ec9658782
MeowKim <ariesmilk51@gmail.com>
2020-04-10 16:57:59 +0900

    separate two step

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,8 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id')->unique();
-            $table->dropPrimary();
-            $table->string('user_id', 20)->primary();
+            $table->id();
+            $table->string('user_id', 20)->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
